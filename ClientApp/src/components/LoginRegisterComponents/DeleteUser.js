da9e82bc6e38791025ef684e22b7cf217511383a
simasnau <simasnau@gmail.com>
2021-04-10 19:37:59 +0300

    Changed style in DeleteUser page

--- ClientApp/src/components/LoginRegisterComponents/DeleteUser.js
+++ ClientApp/src/components/LoginRegisterComponents/DeleteUser.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
 
+let styles={
+    h3Style:{
+        margin:20
+    },
+    btnStyle:{
+        width:"50%",
+    },
+    divStyle:{
+        textAlign:"center"
+    }
+
+}
 
 export default class DeleteUser extends Component {
 
@@ -19,9 +31,9 @@ export default class DeleteUser extends Component {
 
     render() {
         return (
-        <div>
-            <h3>You are about to delete your account. Are you sure you want to do this?</h3>
-            <button type="submit" className="btn btn-primary btn-block" onClick={this.deleteUser}>
+        <div style={styles.divStyle}>
+            <h3 style={styles.h3Style}>You are about to delete your account. Are you sure you want to do this?</h3>
+            <button style={styles.btnStyle} type="submit" className="btn btn-primary" onClick={this.deleteUser}>
             Yes
             </button>
         </div>

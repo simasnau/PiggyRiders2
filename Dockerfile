6050db75ec468c03c3a8260140559b731f69785d
simasnau <simasnau@gmail.com>
2021-04-18 15:21:15 +0300

    Added basic docker support

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+#See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0-buster-slim AS build
+WORKDIR /src
+RUN apt-get update
+RUN curl -sL https://deb.nodesource.com/setup_10.x |  bash -
+RUN apt-get install -y nodejs
+RUN dotnet tool install --global dotnet-ef --version 3.1.2
+ENV PATH="${PATH}:/root/.dotnet/tools"
+COPY ["SmartSaver.csproj", "."]
+RUN dotnet restore "SmartSaver.csproj"
+COPY . .
+WORKDIR "/src"
+RUN dotnet publish "SmartSaver.csproj" -c Release -o /src/publish
+EXPOSE 5000
+EXPOSE 5001
+WORKDIR /src/publish
+CMD ["dotnet", "ef", "--project", "../Persistence", "database", "update"]
+ENTRYPOINT ["dotnet", "SmartSaver.dll", "--urls", "http://*:5000", "https://*:5001"]
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartSaver.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,27 @@ namespace SmartSaver
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var dbContext = services.GetRequiredService<UserContext>();
+                if (dbContext.Database.IsSqlServer())
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
